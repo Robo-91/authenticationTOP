b6de0bf408c10e20a2c57a0b8b994ca42ece2440
Robert Palmer <r91palmer@gmail.com>
2020-12-17 15:08:33 -0600

    Added logic to render authors to message list page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const Message = require("../models/message");
 const User = require("../models/user");
 
-const secretMessage = process.env.SECRET;
-
 exports.message_list = async (req, res) => {
+	const currentUser = req.user;
 	try {
 		const messages = await Message.find();
+		const authors = await User.find({ id: messages.owner });
+		console.log(typeof messages[0].owner.toString());
+		console.log(typeof authors[0].id);
 
-		res.render("index", { title: "Posts", messages });
+		res.render("index", { title: "Posts", messages, currentUser, authors });
 	} catch (e) {
 		console.log(e);
 	}
@@ -41,11 +43,23 @@ exports.message_post = async (req, res) => {
 
 // secret message page GET
 exports.secret_message_get = (req, res) => {
-	console.log(secretMessage);
 	res.render("secret", { title: "Secret Form" });
 };
 
 // secret message page POST
-exports.secret_message_post = (req, res) => {
-	res.render("secret", { title: "secret post" });
+exports.secret_message_post = async (req, res) => {
+	let errors = [];
+	const secretMessage = process.env.SECRET;
+
+	if (req.body.secret === secretMessage) {
+		const user = await User.findById(req.user._id);
+		user.membership_status = true;
+		await user.save();
+		res.redirect("/");
+	} else {
+		errors.push({
+			message: "Incorrect Input for secret message",
+		});
+		res.render("secret", { title: "Secret Form", errors });
+	}
 };
